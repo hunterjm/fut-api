60bf24f2a8fad7f8e5504a847cf358ec92e37689
barczaG <gergo@barcza.hu>
2016-05-25 23:35:40 +0200

    Progress

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import assert from 'assert'
 import utils from './lib/utils'
 import Login from './lib/login'
 import _ from 'underscore'
-import methods from './lib/methods'
+import Methods from './lib/methods'
+import moment from 'moment'
 
 const login = Promise.promisifyAll(new Login())
 
-let Fut = class Fut {
+let Fut = class Fut extends Methods {
   /**
    * [constructor description]
    * @param  {[type]}  options.email          [description]
@@ -20,28 +21,37 @@ let Fut = class Fut {
    * @param  {[type]}  options.tfAuthHandler  [description]
    * @param  {Boolean} options.saveVariable   [description]
    * @param  {Boolean} options.loadVariable   [description]
+   * @param  {Number}  options.RPM            [description]
+   * @param  {Number}  options.minDelay       [description]
    * @return {[type]}                         [description]
    */
   constructor (options) {
+    super()
     assert(options.email, 'Email is required')
     assert(options.password, 'Password is required')
     assert(options.secret, 'Secret is required')
     assert(options.platform, 'Platform is required')
+
+    let defaultOptions = {
+      RPM: 0,
+      minDelay: 0
+    }
+
     this.options = {}
-    Object.assign(this.options, options)
+    Object.assign(this.options, defaultOptions, options)
   }
 
   async loadVariable (key) {
     if (!this.options.loadVariable) return null
-    return this.loadVariable(key)
+    return this.options.loadVariable(key)
   }
 
   async saveVariable (key, val) {
     if (!this.options.saveVariable) return null
-    return this.saveVariable(key, val)
+    return this.options.saveVariable(key, val)
   }
 
-  async init () {
+  async _init () {
     let cookie = await this.loadVariable('cookie')
     if (cookie) {
       login.setCookieJarJSON(cookie)
@@ -49,39 +59,68 @@ let Fut = class Fut {
   }
 
   async login () {
-    let loginResponse = await login.loginAsync(this.email, this.password, this.secret, this.platform, this.tfCodeCb, this.captchaCb)
+    await this._init()
+    let loginResponse = await login.loginAsync(this.options.email, this.options.password, this.options.secret, this.options.platform, this.options.tfAuthHandler, this.options.captchaHandler)
     await this.saveVariable('cookie', login.getCookieJarJSON())
-    this.rawApi = Promise.promisifyAll(loginResponse.apiRequest)
+    this.rawApi = Promise.promisify(loginResponse.apiRequest, loginResponse)
   }
 
-  async sendRequest (url, options) {
+  async api (url, options) {
     var defaultOptions = {
       xHttpMethod: 'GET',
       headers: {}
     }
 
     options = _.extend(defaultOptions, options)
+    options.url = url
+    options.method = 'POST'
 
     options.headers['X-HTTP-Method-Override'] = options.xHttpMethod
     delete options.xHttpMethod
 
-    let {response, body} = await this.rawApi.post(url, options)
+    const {statusCode, statusMessage, body} = await this.rawApi(options)
 
-    if (response.statusCode === 401) throw new Error(response.statusMessage)
-    if (response.statusCode === 404) throw new Error(response.statusMessage)
+    if (statusCode.toString()[0] !== '2') {
+      throw new Error(`FUT api http error: ${statusCode} ${statusMessage}`)
+    }
 
     if (utils.isApiError(body)) {
       body.request = {url, options: options}
-      let err = new Error(JSON.stringify(body))
+      let err = new Error(`Fut api error: ${JSON.stringify(body)}`)
       err.futApiStatusCode = Number(body.code)
       throw err
     }
     return body
   }
+
+  async _limitHandler () {
+    // seconds
+    let sinceLastRequest = moment().diff(this.lastRequestAt)
+    if (sinceLastRequest < this.options.minDelay) {
+      await Promise.delay(this.options.minDelay - sinceLastRequest)
+    }
+
+    // minutes
+    if (moment().diff(this.minuteLimitStartedAt, 'minutes') >= 1 || !this.minuteLimitStartedAt) {
+      this.minuteLimitStartedAt = moment()
+      this.requestsThisMinute = 0
+    } else {
+      this.requestsThisMinute++
+    }
+
+    if (this.requestsThisMinute >= this.options.RPM) {
+      let resetsAt = this.minuteLimitStartedAt.add(1, 'minute')
+      let needsToReset = resetsAt.diff(moment())
+      await Promise.delay(needsToReset)
+    }
+
+    // TODO: continue this
+    this.lastRequestAt = moment()
+  }
 }
 
-Object.assign(Fut.prototype, methods)
-export default Fut
+// Object.assign(Fut.prototype, Methods.prototype)
+module.exports = Fut
 
 // futapi.isPriceValid = utils.isPriceValid
 // futapi.calculateValidPrice = utils.calculateValidPrice
